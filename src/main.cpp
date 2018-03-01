9cc1486964345126690be03e6eed091b0c790a9d
livello <livello@bk.ru>
2018-03-02 01:34:46 +0300

    ds1307 try

--- src/main.cpp
+++ src/main.cpp
@@ -26,29 +26,34 @@ SCK (Serial Clock)  ->  A5 on Uno/Pro-Mini, 21 on Mega2560/Due, 3 Leonardo/Pro-M
 #include <BME280I2C.h>
 #include <Wire.h>
 #include "SPI.h"
+#include <TimeLib.h>
+#include <DS1307RTC.h>
 
 #define SERIAL_BAUD 115200
+
+
+tmElements_t tm;
+
 
 BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
 // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
 
 //////////////////////////////////////////////////////////////////
-void setup()
-{
+void setup() {
+    Serial.println("DS1307RTC Read Test");
+    Serial.println("-------------------");
     Serial.begin(SERIAL_BAUD);
 
-    while(!Serial) {} // Wait
+    while (!Serial) {} // Wait
 
     Wire.begin();
 
-    while(!bme.begin())
-    {
+    while (!bme.begin()) {
         Serial.println("Could not find BME280 sensor!");
         delay(1000);
     }
 
-    switch(bme.chipModel())
-    {
+    switch (bme.chipModel()) {
         case BME280::ChipModel_BME280:
             Serial.println("Found BME280 sensor! Success.");
             break;
@@ -59,12 +64,12 @@ void setup()
             Serial.println("Found UNKNOWN sensor! Error!");
     }
 }
+
 //////////////////////////////////////////////////////////////////
 void printBME280Data
         (
-                Stream* client
-        )
-{
+                Stream *client
+        ) {
     float temp(NAN), hum(NAN), pres(NAN);
 
     BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
@@ -74,7 +79,7 @@ void printBME280Data
 
     client->print("Temp: ");
     client->print(temp);
-    client->print("°"+ String(tempUnit == BME280::TempUnit_Celsius ? 'C' :'F'));
+    client->print("°" + String(tempUnit == BME280::TempUnit_Celsius ? 'C' : 'F'));
     client->print("\t\tHumidity: ");
     client->print(hum);
     client->print("% RH");
@@ -86,26 +91,60 @@ void printBME280Data
 }
 
 //////////////////////////////////////////////////////////////////
-void loop()
-{
-    printBME280Data(&Serial);
-    delay(500);
-}
 
 
 const int RELAY[6] = {8, 9, 10, 11, 12, 13};
 
-void setupRelay()
-{
+void setupRelay() {
 
-  for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < 6; i++) {
 
-    pinMode(RELAY[i], OUTPUT);
+        pinMode(RELAY[i], OUTPUT);
 
-    digitalWrite(RELAY[i], HIGH);
+        digitalWrite(RELAY[i], HIGH);
 
-  }
-  Serial.begin(115200);
+    }
+    Serial.begin(115200);
 }
 
+void print2digits(int number) {
+    if (number >= 0 && number < 10) {
+        Serial.write('0');
+    }
+    Serial.print(number);
+}
 
+void rtc_loop() {
+    if (RTC.read(tm)) {
+        Serial.print("Ok, Time = ");
+        print2digits(tm.Hour);
+        Serial.write(':');
+        print2digits(tm.Minute);
+        Serial.write(':');
+        print2digits(tm.Second);
+        Serial.print(", Date (D/M/Y) = ");
+        Serial.print(tm.Day);
+        Serial.write('/');
+        Serial.print(tm.Month);
+        Serial.write('/');
+        Serial.print(tmYearToCalendar(tm.Year));
+        Serial.println();
+    } else {
+        if (RTC.chipPresent()) {
+            Serial.println("The DS1307 is stopped.  Please run the SetTime");
+            Serial.println("example to initialize the time and begin running.");
+            Serial.println();
+        } else {
+            Serial.println("DS1307 read error!  Please check the circuitry.");
+            Serial.println();
+        }
+        delay(9000);
+    }
+    delay(1000);
+}
+
+void loop() {
+    printBME280Data(&Serial);
+    rtc_loop();
+    delay(500);
+}
