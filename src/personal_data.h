fc8b99dc07da545311ff40ed97f5857f5a6d28b0
livello <livello@bk.ru>
2018-03-02 11:12:32 +0300

    personal_data

---
+++ src/personal_data.h
@@ -0,0 +1,9 @@
+//
+// Created by livello on 28.02.18.
+//
+
+#ifndef PERSONAL_DATA_H
+#define PERSONAL_DATA_H
+
+#endif //VENTILATIONCONTROL_ARDUINODUE_PERSONAL_DATA_H
+#define my_personal_mac_address {0xC4,0x3E,0x11,0x03,0x1B,0x1E}
