b46d1a04622cf41e61106f8b0963cf5534f97762
livello <livello@bk.ru>
2018-03-05 22:42:02 +0300

    ntp

--- src/main.cpp
+++ src/main.cpp
@@ -380,6 +380,7 @@ void getNtpTime() {
         Serial.println("no answer was received");
         return;
     } else {
+        Serial.println("received");
         udp.read(packetBuffer2, NTP_PACKET_SIZE); // read the packet into the buffer
         unsigned long highWord = word(packetBuffer2[40], packetBuffer2[41]);
         unsigned long lowWord = word(packetBuffer2[42], packetBuffer2[43]);
@@ -398,22 +399,6 @@ void getNtpTime() {
 // send an NTP request to the time server at the given address
 void sendNTPpacket(IPAddress &address) {
     Serial.println("sending NTP packet...");
-    // set all bytes in the buffer to 0
-//    memset(packetBuffer, 0, NTP_PACKET_SIZE);
-//    // Initialize values needed to form NTP request
-//    // (see URL above for details on the packets)
-//    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-//    packetBuffer[1] = 0;     // Stratum, or type of clock
-//    packetBuffer[2] = 6;     // Polling Interval
-//    packetBuffer[3] = 0xEC;  // Peer Clock Precision
-//    // 8 bytes of zero for Root Delay & Root Dispersion
-//    packetBuffer[12] = 49;
-//    packetBuffer[13] = 0x4E;
-//    packetBuffer[14] = 49;
-//    packetBuffer[15] = 52;
-
-    // all NTP fields have been given values, now
-    // you can send a packet requesting a timestamp:
     udp.beginPacket(address, 123); //NTP requests are to port 123
     udp.write(packetBuffer, NTP_PACKET_SIZE);
     udp.endPacket();
