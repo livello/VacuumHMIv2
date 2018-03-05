0a670b8c44ed69c198b25b558a99c339d480480c
livello <livello@bk.ru>
2018-03-05 18:29:21 +0300

    ds18b20

--- src/main.cpp
+++ src/main.cpp
@@ -110,7 +110,7 @@ void ethernet_loop() {
                     client.println("Content-Type: text/html");
                     client.println(
                             "Connection: close");  // the connection will be closed after completion of the response
-                    client.println("Refresh: 25");  // refresh the page automatically every 5 sec
+                    client.println("Refresh: 15");
                     client.println();
                     client.println("<!DOCTYPE HTML>");
                     client.println("<html>");
@@ -166,88 +166,86 @@ void ds18b20Read(Stream *stream) {
     byte type_s;
     byte data[12];
     byte addr[8];
+    while(ds.search(addr)) {
 
 
-    if (!ds.search(addr)) {
-        stream->println("No more addresses.");
-        ds.reset_search();
-        return;
-    }
-
-    Serial.print("ROM =");
-    for (i = 0; i < 8; i++) {
-        stream->write(' ');
-        stream->print(addr[i], HEX);
-    }
-
-    if (OneWire::crc8(addr, 7) != addr[7]) {
-        stream->println("CRC is not valid!");
-        return;
-    }
-
-    // the first ROM byte indicates which chip
-    switch (addr[0]) {
-        case 0x10:
-            stream->print("  Chip = DS18S20");  // or old DS1820
-            type_s = 1;
-            break;
-        case 0x28:
-            stream->print("  Chip = DS18B20");
-            type_s = 0;
-            break;
-        case 0x22:
-            stream->print("  Chip = DS1822");
-            type_s = 0;
-            break;
-        default:
-            stream->print("Device is not a DS18x20 family device.");
-            return;
-    }
-    ds.reset();
-    ds.select(addr);
-    ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-
-    chThdSleepMilliseconds(1000);
-    // we might do a ds.depower() here, but the reset will take care of it.
-
-    present = ds.reset();
-    ds.select(addr);
-    ds.write(0xBE);         // Read Scratchpad
-
-    stream->print("  Data = ");
-    stream->print(present, HEX);
-    stream->print(" ");
-    for (i = 0; i < 9; i++) {           // we need 9 bytes
-        data[i] = ds.read();
-        stream->print(data[i], HEX);
-        stream->print(" ");
-    }
-    stream->print(" CRC=");
-    stream->print(OneWire::crc8(data, 8), HEX);
-
-    // Convert the data to actual temperature
-    // because the result is a 16 bit signed integer, it should
-    // be stored to an "int16_t" type, which is always 16 bits
-    // even when compiled on a 32 bit processor.
-    int16_t raw = (data[1] << 8) | data[0];
-    if (type_s) {
-        raw = raw << 3; // 9 bit resolution default
-        if (data[7] == 0x10) {
-            // "count remain" gives full 12 bit resolution
-            raw = (raw & 0xFFF0) + 12 - data[6];
+        stream->print("ROM =");
+        for (i = 0; i < 8; i++) {
+            stream->write(' ');
+            stream->print(addr[i], HEX);
         }
-    } else {
-        byte cfg = (data[4] & 0x60);
-        // at lower res, the low bits are undefined, so let's zero them
-        if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-        else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-        else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-        //// default is 12 bit resolution, 750 ms conversion time
+
+        if (OneWire::crc8(addr, 7) != addr[7]) {
+            stream->println("CRC is not valid!");
+            return;
+        }
+
+        // the first ROM byte indicates which chip
+        switch (addr[0]) {
+            case 0x10:
+                stream->print("  Chip = DS18S20");  // or old DS1820
+                type_s = 1;
+                break;
+            case 0x28:
+                stream->print("  Chip = DS18B20");
+                type_s = 0;
+                break;
+            case 0x22:
+                stream->print("  Chip = DS1822");
+                type_s = 0;
+                break;
+            default:
+                stream->print("Device is not a DS18x20 family device.");
+                return;
+        }
+        ds.reset();
+        ds.select(addr);
+        ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+
+        chThdSleepMilliseconds(1000);
+        // we might do a ds.depower() here, but the reset will take care of it.
+
+        present = ds.reset();
+        ds.select(addr);
+        ds.write(0xBE);         // Read Scratchpad
+
+        stream->print("  Data = ");
+        stream->print(present, HEX);
+        stream->print(" ");
+        for (i = 0; i < 9; i++) {           // we need 9 bytes
+            data[i] = ds.read();
+            stream->print(data[i], HEX);
+            stream->print(" ");
+        }
+        stream->print(" CRC=");
+        stream->print(OneWire::crc8(data, 8), HEX);
+
+        // Convert the data to actual temperature
+        // because the result is a 16 bit signed integer, it should
+        // be stored to an "int16_t" type, which is always 16 bits
+        // even when compiled on a 32 bit processor.
+        int16_t raw = (data[1] << 8) | data[0];
+        if (type_s) {
+            raw = raw << 3; // 9 bit resolution default
+            if (data[7] == 0x10) {
+                // "count remain" gives full 12 bit resolution
+                raw = (raw & 0xFFF0) + 12 - data[6];
+            }
+        } else {
+            byte cfg = (data[4] & 0x60);
+            // at lower res, the low bits are undefined, so let's zero them
+            if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+            else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+            else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+            //// default is 12 bit resolution, 750 ms conversion time
+        }
+        ds18b20ClockTemperature = (float) raw / 16.0;
+        stream->print("  DS18b20Temp = ");
+        stream->print(ds18b20ClockTemperature);
+        stream->print(" C, \n");
     }
-    ds18b20ClockTemperature = (float) raw / 16.0;
-    stream->print("  DS18b20Temp = ");
-    stream->print(ds18b20ClockTemperature);
-    stream->print(" C, ");
+    stream->println("No more addresses.");
+    ds.reset_search();
 }
 //////////////////////////////////////////////////////////////////
 
