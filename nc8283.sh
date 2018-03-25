cbc8d21ab70cf4e3d7b10a94192477174953b91d
livello <livello@bk.ru>
2018-03-25 17:32:41 +0300

    app test

---
+++ nc8283.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+while true;
+do nc -lvvv -p 8283;
+done
--- src/main.cpp
+++ src/main.cpp
@@ -24,10 +24,12 @@ float temp_DS18B20;
 float humidity_DHT22 = 0, temp_DHT22 = 0;
 OneWire dallas18b20(ONEWIRE_PIN);
 
-const unsigned long postingInterval = 330000;  // интервал между отправками данных в миллисекундах (10 минут)
-uint32_t nextConnectionTime = 20000;           // время последней передачи данных
+//const unsigned long postingInterval = 330000;  // интервал между отправками данных в миллисекундах (10 минут)
+const unsigned long postingInterval = 1000;  // интервал между отправками данных в миллисекундах (10 минут)
+uint32_t nextConnectionTime = 3000;           // время последней передачи данных
 char replyBuffer[160];
-const char *narodmon_host = "narodmon.ru";
+//const char *narodmon_host = "narodmon.ru";
+const char *narodmon_host = "192.168.0.115";
 const int narodmon_port = 8283;
 char numberConversionBuffer[6];
 
